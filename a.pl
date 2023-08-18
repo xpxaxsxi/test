58498ed4f2073aafcf8868b857f29fd604e4bb9b
xpxaxsxi <pasikuuskasi@windowslive.com>
2023-08-18 12:05:11 +0300

    Module comment

--- a.pl
+++ a.pl
@@ -1,4 +1,9 @@
 :-module(wasmtst,[x/1]).
+\** <module> Simple test module for wasm in browser
+Usage: 
+=|load_files( 'https://raw.githubusercontent.com/xpxaxsxi/test/main/a.pl',[]).|=
+
+\*
 x(hello).
 x(world).
 
