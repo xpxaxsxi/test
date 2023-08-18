bad3d500782c621e2742bcfef5a41d6049a3eb6a
xpxaxsxi <pasikuuskasi@windowslive.com>
2023-08-18 13:28:49 +0300

    in wasm shell demo
    In iPhone Firefox

--- a.pl
+++ a.pl
@@ -1,5 +1,5 @@
 :-module(wasmtst,[x/1]).
-\** <module> Simple test module for wasm in browser
+\** <module> Simple test module for wasm in browser (in the wasm shell demo).
 Usage: 
 =|load_files( 'https://raw.githubusercontent.com/xpxaxsxi/test/main/a.pl',[]).|=
 
