3be4587f82e1c363db3fe7b22cbd018fc0b188d4
xpxaxsxi <pasikuuskasi@windowslive.com>
2023-08-19 15:10:24 +0300

    Update a.pl
    A really stupid mistake :)

--- a.pl
+++ a.pl
@@ -1,10 +1,9 @@
 :-module(wasmtst,[x/1]).
-\** <module> Simple test module for wasm in browser (in the wasm shell demo)
+/** <module> Simple test module for wasm in browser (in the wasm shell demo)
 
 Usage: 
 =|load_files( 'https://raw.githubusercontent.com/xpxaxsxi/test/main/a.pl',[]).|=.
-
-*\
+*/
 
 x(hello).
 x(world).
